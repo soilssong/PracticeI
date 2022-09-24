c6e8cc3935b9f74f1bdf9de5a339454c10d72e39
Mertcan Öz <mertcan6506@hotmail.com>
2022-09-24 15:16:51 +0300

    Basic PopUp implementation

---
+++ Assets/Scripts/Popup.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class Popup : MonoBehaviour
+
+
+{
+
+    public GameObject PopUpBox;
+    public TextMeshProUGUI PopUpText;
+    public string text;
+    public bool playerInrange;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInrange = true;
+            Debug.Log("entered");
+            if (PopUpBox.activeInHierarchy)
+            {
+                PopUpBox.SetActive(false);
+            }
+            else
+            {
+                PopUpBox.SetActive(true);
+                PopUpText.text = text;
+            }
+
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInrange = false;
+            Debug.Log("exited");
+            PopUpBox.SetActive(false);
+
+
+
+        }
+
+
+    }
+}
